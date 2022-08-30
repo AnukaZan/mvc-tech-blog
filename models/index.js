bd50fd130eb90334fb480656efd0bcd53ae38f96
Anuka <anukazan@gmail.com>
2022-08-29 22:55:50 -0700

    edit model relationships, add JS and SS

--- models/index.js
+++ models/index.js
@@ -14,22 +14,31 @@ Post.belongsTo(User, {
 
 // comment belongs to user
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
 });
 
 // comment belongs to post
 Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks: true
 });
 
 // user has many comments
 User.hasMany(Comment, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
 });
+
 
 // post has many comments
 Post.hasMany(Comment, {
-    foreignKey: 'post_id'
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks:true
 })
 
 module.exports = { User, Post, Comment }; 
